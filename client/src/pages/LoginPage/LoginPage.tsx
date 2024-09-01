b200b6dd15a198fc3e1d8c2ddf613184d3fee7d6
Davit Gelovani <davit.gelovani403@gmail.com>
2024-09-02 01:19:50 +0400

    #1 - Success/Fail messages

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { API_URL } from "../../utils/http";
 import { LoginPageContainer, LoginPageWrapper } from "./loginpage.styled";
 import { Button, Divider, Form, FormProps, Input } from "antd";
+import useMessage from "antd/es/message/useMessage";
 
 type fieldTypes = {
   email: string;
@@ -9,51 +10,68 @@ type fieldTypes = {
 };
 
 function LoginPage() {
+  const [messageApi, contextHolder] = useMessage();
+
   const onFinish: FormProps<fieldTypes>["onFinish"] = (values) => {
-    axios.post(API_URL + "/auth/login", {
-      email: values.email,
-      password: values.password,
-    });
+    axios
+      .post(API_URL + "/auth/login", {
+        email: values.email,
+        password: values.password,
+      })
+      .then(() => {
+        messageApi.open({
+          type: "success",
+          content: "User logged in successfully",
+        });
+      })
+      .catch(() => {
+        messageApi.open({
+          type: "error",
+          content: "User login failed",
+        });
+      });
   };
   return (
-    <LoginPageWrapper>
-      <LoginPageContainer>
-        <Form onFinish={onFinish}>
-          <h1>Sign in to eConference</h1>
-          <Divider />
-          <label>Email</label>
-          <Form.Item<fieldTypes>
-            rules={[
-              {
-                type: "email",
-                required: true,
-                message: "Please input your email!",
-              },
-            ]}
-            name={"email"}
-          >
-            <Input placeholder="Enter your email address" />
-          </Form.Item>
-          <label>Password</label>
-          <Form.Item<fieldTypes>
-            name={"password"}
-            rules={[
-              {
-                required: true,
-                message: "Please input your password!",
-              },
-            ]}
-          >
-            <Input placeholder="Enter your password" type="password" />
-          </Form.Item>
-          <a>Forgot password?</a>
+    <>
+      <LoginPageWrapper>
+        <LoginPageContainer>
+          <Form onFinish={onFinish}>
+            <h1>Sign in to eConference</h1>
+            <Divider />
+            <label>Email</label>
+            <Form.Item<fieldTypes>
+              rules={[
+                {
+                  type: "email",
+                  required: true,
+                  message: "Please input your email!",
+                },
+              ]}
+              name={"email"}
+            >
+              <Input placeholder="Enter your email address" />
+            </Form.Item>
+            <label>Password</label>
+            <Form.Item<fieldTypes>
+              name={"password"}
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input placeholder="Enter your password" type="password" />
+            </Form.Item>
+            <a>Forgot password?</a>
 
-          <Button type="primary" htmlType="submit">
-            Continue with Email
-          </Button>
-        </Form>
-      </LoginPageContainer>
-    </LoginPageWrapper>
+            <Button type="primary" htmlType="submit">
+              Continue with Email
+            </Button>
+          </Form>
+        </LoginPageContainer>
+      </LoginPageWrapper>
+    </>
   );
 }
 
--- client/src/pages/LoginPage/RegisterPage.tsx
+++ client/src/pages/LoginPage/RegisterPage.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { LoginPageContainer, LoginPageWrapper } from "./loginpage.styled";
 import { Button, Divider, Form, FormProps, Input } from "antd";
 import { API_URL } from "../../utils/http";
+import useMessage from "antd/es/message/useMessage";
 
 type fieldTypes = {
   username: string;
@@ -12,81 +13,98 @@ type fieldTypes = {
 
 function RegisterPage() {
   const [form] = Form.useForm();
+  const [messageApi, contextHolder] = useMessage();
 
   const onFinish: FormProps<fieldTypes>["onFinish"] = (values) => {
-    axios.post(API_URL + "/auth/register", {
-      email: values.email,
-      password: values.password,
-      username: values.username,
-    });
+    axios
+      .post(API_URL + "/auth/register", {
+        email: values.email,
+        password: values.password,
+        username: values.username,
+      })
+      .then(() => {
+        messageApi.open({
+          type: "success",
+          content: "User registered successfully",
+        });
+      })
+      .catch(() => {
+        messageApi.open({
+          type: "error",
+          content: "User registration failed",
+        });
+      });
   };
 
   return (
-    <LoginPageWrapper>
-      <LoginPageContainer>
-        <Form onFinish={onFinish} name="register" form={form}>
-          <h1>Sign up to eConference</h1>
-          <Divider />
-          <label>Username</label>
-          <Form.Item<fieldTypes>
-            rules={[
-              {
-                required: true,
-                message: "Please input your username!",
-              },
-            ]}
-            name={"username"}
-          >
-            <Input placeholder="Enter your username" />
-          </Form.Item>
-          <label>Email</label>
-          <Form.Item<fieldTypes>
-            rules={[
-              {
-                type: "email",
-                required: true,
-                message: "Please input your email!",
-              },
-            ]}
-            name={"email"}
-          >
-            <Input placeholder="Enter your email address" />
-          </Form.Item>
-          <label>Password</label>
-          <Form.Item<fieldTypes>
-            name={"password"}
-            rules={[
-              {
-                required: true,
-                message: "Please input your password!",
-              },
-            ]}
-          >
-            <Input placeholder="Enter your password" type="password" />
-          </Form.Item>
-          <label>Repeat Password</label>
-          <Form.Item<fieldTypes>
-            name={"repeatPassword"}
-            rules={[
-              {
-                validator(_, value) {
-                  if (value !== form.getFieldValue("password")) {
-                    return Promise.reject("Passwords do not match");
-                  }
-                  return Promise.resolve();
+    <>
+      {contextHolder}
+      <LoginPageWrapper>
+        <LoginPageContainer>
+          <Form onFinish={onFinish} name="register" form={form}>
+            <h1>Sign up to eConference</h1>
+            <Divider />
+            <label>Username</label>
+            <Form.Item<fieldTypes>
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
                 },
-              },
-            ]}
-          >
-            <Input placeholder="Repeat your password" type="password" />
-          </Form.Item>
+              ]}
+              name={"username"}
+            >
+              <Input placeholder="Enter your username" />
+            </Form.Item>
+            <label>Email</label>
+            <Form.Item<fieldTypes>
+              rules={[
+                {
+                  type: "email",
+                  required: true,
+                  message: "Please input your email!",
+                },
+              ]}
+              name={"email"}
+            >
+              <Input placeholder="Enter your email address" />
+            </Form.Item>
+            <label>Password</label>
+            <Form.Item<fieldTypes>
+              name={"password"}
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input placeholder="Enter your password" type="password" />
+            </Form.Item>
+            <label>Repeat Password</label>
+            <Form.Item<fieldTypes>
+              name={"repeatPassword"}
+              rules={[
+                {
+                  validator(_, value) {
+                    if (value !== form.getFieldValue("password")) {
+                      return Promise.reject("Passwords do not match");
+                    }
+                    return Promise.resolve();
+                  },
+                },
+              ]}
+            >
+              <Input placeholder="Repeat your password" type="password" />
+            </Form.Item>
 
-          <Button type="primary" htmlType="submit">
-            Continue with Email
-          </Button>
-        </Form>
-      </LoginPageContainer>
-    </LoginPageWrapper>
+            <Button type="primary" htmlType="submit">
+              Continue with Email
+            </Button>
+          </Form>
+        </LoginPageContainer>
+      </LoginPageWrapper>
+    </>
   );
 }
 
